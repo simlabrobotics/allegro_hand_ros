a1746fc765fe2e3c0c011c07a5367469a63de851
Felix Duvallet <felix.duvallet@epfl.ch>
2015-08-21 15:25:01 +0200

    whitespace.

--- allegro_hand_keyboard/src/allegro_hand_keyboard.cpp
+++ allegro_hand_keyboard/src/allegro_hand_keyboard.cpp
@@ -19,14 +19,14 @@ using namespace std;
 // http://nehe.gamedev.net/article/msdn_virtualkey_codes/15009/
 #define VK_SPACE 0x20
 #define VK_LSHIFT 0xA0
- 
 
-#define KEYCODE_RIGHT 0x43 
+
+#define KEYCODE_RIGHT 0x43
 #define KEYCODE_LEFT 0x44
 #define KEYCODE_UP 0x41
 #define KEYCODE_DOWN 0x42
 
-#define KEYCODE_0	0x30	
+#define KEYCODE_0	0x30
 #define KEYCODE_1	0x31
 #define KEYCODE_2	0x32
 #define KEYCODE_3	0x33
@@ -104,25 +104,25 @@ using namespace std;
 
 class AHKeyboard
 {
-public:
-	AHKeyboard();
-	void keyLoop();
+ public:
+  AHKeyboard();
+  void keyLoop();
 
-private:
+ private:
 
-  
-	ros::NodeHandle nh_;
-	int count_;
-	float edit_;
-	ros::Publisher cmd_pub_;
-  
+
+  ros::NodeHandle nh_;
+  int count_;
+  float edit_;
+  ros::Publisher cmd_pub_;
+
 };
 
-	AHKeyboard::AHKeyboard():
-	count_(0),
-	edit_(0.0)
+AHKeyboard::AHKeyboard():
+    count_(0),
+    edit_(0.0)
 {
-	cmd_pub_ = nh_.advertise<std_msgs::String>("/allegroHand/lib_cmd", 10);
+  cmd_pub_ = nh_.advertise<std_msgs::String>("/allegroHand/lib_cmd", 10);
 }
 
 
@@ -136,70 +136,70 @@ struct termios cooked, raw;
 
 void quit(int sig)
 {
-	tcsetattr(kfd, TCSANOW, &cooked);
-	ros::shutdown();
-	exit(0);
+  tcsetattr(kfd, TCSANOW, &cooked);
+  ros::shutdown();
+  exit(0);
 }
 
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "allegro_hand_keyboard_cmd");
-  	AHKeyboard allegro_hand_keyboard_cmd;
+  ros::init(argc, argv, "allegro_hand_keyboard_cmd");
+  AHKeyboard allegro_hand_keyboard_cmd;
 
-  	signal(SIGINT,quit);
+  signal(SIGINT,quit);
 
-  	allegro_hand_keyboard_cmd.keyLoop();
-  
-  	return(0);
+  allegro_hand_keyboard_cmd.keyLoop();
+
+  return(0);
 }
 
 
 void AHKeyboard::keyLoop()
 {
-  	char c;
-  	bool dirty=false;
-  	bool jointUpdate=false;
+  char c;
+  bool dirty=false;
+  bool jointUpdate=false;
 
 
-  	// get the console in raw mode                                                              
-  	tcgetattr(kfd, &cooked);
-  	memcpy(&raw, &cooked, sizeof(struct termios));
-  	raw.c_lflag &=~ (ICANON | ECHO);
-  	// Setting a new line, then end of file                         
-  	raw.c_cc[VEOL] = 1;
-  	raw.c_cc[VEOF] = 2;
-  	tcsetattr(kfd, TCSANOW, &raw);
+  // get the console in raw mode
+  tcgetattr(kfd, &cooked);
+  memcpy(&raw, &cooked, sizeof(struct termios));
+  raw.c_lflag &=~ (ICANON | ECHO);
+  // Setting a new line, then end of file
+  raw.c_cc[VEOL] = 1;
+  raw.c_cc[VEOF] = 2;
+  tcsetattr(kfd, TCSANOW, &raw);
 
-	sleep(2);
-	std::cout << std::endl;
-	std::cout << " -----------------------------------------------------------------------------" << std::endl;
-  	std::cout << "  Use the keyboard to send Allegro Hand grasp & motion commands" << std::endl;
-  	std::cout << " -----------------------------------------------------------------------------" << std::endl;
-	
-  	std::cout << "\tHome Pose:\t\t\t'H'" << std::endl;  
-  	std::cout << "\tReady Pose:\t\t\t'R'" << std::endl; 
-  	std::cout << "\tPinch (index+thumb):\t\t'P'" << std::endl; 
-  	std::cout << "\tPinch (middle+thumb):\t\t'M'" << std::endl; 
-  	std::cout << "\tGrasp (3 fingers):\t\t'G'" << std::endl; 
-  	std::cout << "\tGrasp (4 fingers):\t\t'F'" << std::endl;  
-  	std::cout << "\tGrasp (envelop):\t\t'E'" << std::endl;  
-  	std::cout << "\tMotors Off (free motion):\t'O'" << std::endl;  	
-  	std::cout << "\tSave Current Pose:\t\t'S'" << std::endl;  
-  	std::cout << "\tPD Control (last saved):\t'Space'" << std::endl;   		
-	std::cout << " -----------------------------------------------------------------------------" << std::endl;  	  	
-	//std::cout << "  Note: Unless elsewhere implemented, these keyboard commands only work with " << std::endl;    
-	//std::cout << "  the 'allegro_hand_core_grasp' and 'allegro_hand_core_grasp_slp' packages." << std::endl;	  	
-	std::cout << "  Subscriber code for reading these messages is included in '~core_template'." << std::endl;		
-	std::cout << " -----------------------------------------------------------------------------\n" << std::endl;  		  	
+  sleep(2);
+  std::cout << std::endl;
+  std::cout << " -----------------------------------------------------------------------------" << std::endl;
+  std::cout << "  Use the keyboard to send Allegro Hand grasp & motion commands" << std::endl;
+  std::cout << " -----------------------------------------------------------------------------" << std::endl;
+
+  std::cout << "\tHome Pose:\t\t\t'H'" << std::endl;
+  std::cout << "\tReady Pose:\t\t\t'R'" << std::endl;
+  std::cout << "\tPinch (index+thumb):\t\t'P'" << std::endl;
+  std::cout << "\tPinch (middle+thumb):\t\t'M'" << std::endl;
+  std::cout << "\tGrasp (3 fingers):\t\t'G'" << std::endl;
+  std::cout << "\tGrasp (4 fingers):\t\t'F'" << std::endl;
+  std::cout << "\tGrasp (envelop):\t\t'E'" << std::endl;
+  std::cout << "\tMotors Off (free motion):\t'O'" << std::endl;
+  std::cout << "\tSave Current Pose:\t\t'S'" << std::endl;
+  std::cout << "\tPD Control (last saved):\t'Space'" << std::endl;
+  std::cout << " -----------------------------------------------------------------------------" << std::endl;
+  //std::cout << "  Note: Unless elsewhere implemented, these keyboard commands only work with " << std::endl;
+  //std::cout << "  the 'allegro_hand_core_grasp' and 'allegro_hand_core_grasp_slp' packages." << std::endl;
+  std::cout << "  Subscriber code for reading these messages is included in '~core_template'." << std::endl;
+  std::cout << " -----------------------------------------------------------------------------\n" << std::endl;
 
   for(;;)
   {
-  
-	std_msgs::String msg;
+
+    std_msgs::String msg;
     std::stringstream ss;
-  
-    // get the next event from the keyboard  
+
+    // get the next event from the keyboard
     if(read(kfd, &c, 1) < 0)
     {
       perror("read():");
@@ -210,79 +210,79 @@ void AHKeyboard::keyLoop()
     ROS_DEBUG("value: 0x%02X\n", c);
     printf("value: 0x%02X\n", c);
 
-	switch(c)
+    switch(c)
     {
-	case VK_SPACE:
-        	ROS_DEBUG("space bar: PD Control");
-        	//cmd_ = 4;
-        	ss << "pdControl";
-        	dirty = true;
-        	break;
-      	case KEYCODE_h:
-        	ROS_DEBUG("h_key: Home");
-        	//cmd_ = 1;
-        	ss << "home";
-        	dirty = true;
-        	break;
-      	case KEYCODE_r:
-        	ROS_DEBUG("r_key: Ready");
-        	//cmd_ = 2;
-        	ss << "ready";
-        	dirty = true;
-        	break;
-      	case KEYCODE_g:
-        	ROS_DEBUG("g_key: Grasp (3 finger)");
-        	//cmd_ = 3;
-        	ss << "grasp_3";
-        	dirty = true;
-        	break;	
-      	case KEYCODE_f:
-        	ROS_DEBUG("f_key: Grasp (4 finger)");
-        	//cmd_ = 3;
-        	ss << "grasp_4";
-        	dirty = true;
-        	break;	        	
-      	case KEYCODE_p:
-        	ROS_DEBUG("p_key: Pinch (index)");
-        	//cmd_ = 4;
-        	ss << "pinch_it";
-        	dirty = true;
-        	break;  
-      	case KEYCODE_m:
-        	ROS_DEBUG("m_key: Pinch (middle)");
-        	//cmd_ = 4;
-        	ss << "pinch_mt";
-        	dirty = true;
-        	break;  
-	case KEYCODE_e:
-        	ROS_DEBUG("e_key: Envelop");
-        	//cmd_ = 4;
-        	ss << "envelop";
-        	dirty = true;
-        	break;  
-	case KEYCODE_o:
-        	ROS_DEBUG("o_key: Servos Off");
-        	//cmd_ = 4;
-        	ss << "off";
-        	dirty = true;
-        	break; 
-	case KEYCODE_s:
-        	ROS_DEBUG("s_key: Save joint pos. for PD control");
-        	//cmd_ = 4;
-        	ss << "save";
-        	dirty = true;
-        	break;   	      	        	
+      case VK_SPACE:
+        ROS_DEBUG("space bar: PD Control");
+        //cmd_ = 4;
+        ss << "pdControl";
+        dirty = true;
+        break;
+      case KEYCODE_h:
+        ROS_DEBUG("h_key: Home");
+        //cmd_ = 1;
+        ss << "home";
+        dirty = true;
+        break;
+      case KEYCODE_r:
+        ROS_DEBUG("r_key: Ready");
+        //cmd_ = 2;
+        ss << "ready";
+        dirty = true;
+        break;
+      case KEYCODE_g:
+        ROS_DEBUG("g_key: Grasp (3 finger)");
+        //cmd_ = 3;
+        ss << "grasp_3";
+        dirty = true;
+        break;
+      case KEYCODE_f:
+        ROS_DEBUG("f_key: Grasp (4 finger)");
+        //cmd_ = 3;
+        ss << "grasp_4";
+        dirty = true;
+        break;
+      case KEYCODE_p:
+        ROS_DEBUG("p_key: Pinch (index)");
+        //cmd_ = 4;
+        ss << "pinch_it";
+        dirty = true;
+        break;
+      case KEYCODE_m:
+        ROS_DEBUG("m_key: Pinch (middle)");
+        //cmd_ = 4;
+        ss << "pinch_mt";
+        dirty = true;
+        break;
+      case KEYCODE_e:
+        ROS_DEBUG("e_key: Envelop");
+        //cmd_ = 4;
+        ss << "envelop";
+        dirty = true;
+        break;
+      case KEYCODE_o:
+        ROS_DEBUG("o_key: Servos Off");
+        //cmd_ = 4;
+        ss << "off";
+        dirty = true;
+        break;
+      case KEYCODE_s:
+        ROS_DEBUG("s_key: Save joint pos. for PD control");
+        //cmd_ = 4;
+        ss << "save";
+        dirty = true;
+        break;
     }
-   
+
 
     if(dirty ==true)
     {
       msg.data = ss.str();
       ROS_INFO("%s", msg.data.c_str());
-      cmd_pub_.publish(msg);    
+      cmd_pub_.publish(msg);
       dirty=false;
     }
-    
+
   }
 
 
